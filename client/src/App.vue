67317e7ae068cc3fbd915fa59a264c58bbfbd64f
Elena Rocha <ele_r.v@hotmail.com>
2024-02-27 15:17:17 +0100

    recibir los mensajes en el front

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import { io } from 'socket.io-client';
+import { onBeforeMount, ref } from 'vue';
 
+const socket = io('http://localhost:3001');
+
+const messages = ref([]);
+
+onBeforeMount(() => {
+  socket.emit('findAllMessages', {}, (response: any) => {
+    messages.value = response;
+  })
+});
 </script>
 
 <template>
-  <div></div>
+  <div class="chat"></div>
 </template>
 
 <style scoped>
