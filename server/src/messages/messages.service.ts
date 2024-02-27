c59b97e44416c506b483c07b3663d86c97d66739
Elena Rocha <ele_r.v@hotmail.com>
2024-02-27 13:44:13 +0100

    array para guardar los mensajes

--- server/src/messages/messages.service.ts
+++ server/src/messages/messages.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
+  messages: Message[] = [{ name: 'Elena', text: 'Hola' }];
+
   create(createMessageDto: CreateMessageDto) {
     return 'This action adds a new message';
   }
 
   findAll() {
-    return `This action returns all messages`;
+    return this.messages;
   }
 }
