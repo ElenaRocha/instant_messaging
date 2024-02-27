68d5878e36806225b8918efd2133b6ddc5cfe27f
Elena Rocha <ele_r.v@hotmail.com>
2024-02-27 13:33:46 +0100

    añadir cors

--- server/src/messages/messages.gateway.ts
+++ server/src/messages/messages.gateway.ts
@@ -1,9 +1,17 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class MessagesGateway {
   constructor(private readonly messagesService: MessagesService) {}
 
