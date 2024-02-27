7fc08dfa7287a21c5becc155000c8d414a3632af
Elena Rocha <ele_r.v@hotmail.com>
2024-02-27 19:21:34 +0100

    métodos en el cliente

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,58 @@
-<script setup lang="ts">
+<script setup>
+// TODO: convertir a typescript
 import { io } from 'socket.io-client';
 import { onBeforeMount, ref } from 'vue';
 
 const socket = io('http://localhost:3001');
 
 const messages = ref([]);
+const messageText = ref('');
+const joined = ref(false);
+const name = ref('');
+const typingDisplay = ref('');
 
 onBeforeMount(() => {
-  socket.emit('findAllMessages', {}, (response: any) => {
+  socket.emit('findAllMessages', {}, (response) => {
+    console.log(typeof response)
     messages.value = response;
-  })
+  });
+
+  socket.on('message', (message) => {
+    messages.value.push(message);
+  });
 });
+
+const join = () => {
+  socket.emit('join', { name: name.value }, () => {
+    joined.value = true;
+  });
+};
+
+const sendMessage = () => {
+  socket.emit('createMessage', { text: messageText.value }, () => {
+    messageText.value = '';
+  });
+};
+
+let timeout;
+const emitTyping = () => {
+  socket.emit('typing', {isTyping: true});
+  timeout = setTimeout(() => {
+    socket.emit('typing', {isTyping:false});
+  }, 2000);
+};
 </script>
 
 <template>
-  <div class="chat"></div>
+  <div class="chat">
+    <div class="chat-container">
+      <div class="messages-container">
+        <div v-for="message in messages" :key="message.id">
+          [{{ message.name }}]: {{ message.text }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
